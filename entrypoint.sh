90d439843c43a0e8e30da505ffe25212126df8c3
yibo-yuka <chu98672@gmail.com>
2026-04-08 01:54:08 +0800

    不使用netcat, 改用python socket套件嘗試連接資料庫確認是否可連線

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
-# 等待資料庫就緒 (這是一個簡單的循環檢查)
 echo "Waiting for postgres..."
-while ! nc -z db 5432; do
-  sleep 0.1
-done
+
+# 使用 python 嘗試連接資料庫，直到成功為止
+# 這樣就不需要安裝 nc 了
+python << END
+import socket
+import time
+import os
+
+host = os.environ.get('DB_HOST', 'db')
+port = int(os.environ.get('DB_PORT', 5432))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while True:
+    try:
+        s.connect((host, port))
+        s.close()
+        break
+    except socket.error:
+        time.sleep(0.1)
+END
+
 echo "PostgreSQL started"
 
 # 執行遷移
